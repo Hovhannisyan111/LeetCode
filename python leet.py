e6866e2fdf26e3e63b812f4402c9781c5fd56f9e
Hovhannisyan111 <ogannisian.2004.01@gmail.com>
2024-04-03 23:51:43 +0400

    Leetcode

--- python leet.py
+++ python leet.py
@@ -65,58 +65,23 @@
 #print(par('[(])'))
 
 #LeetCode 28
-#
-#def get_index(haystack, needle):
-#    if needle not in haystack:
-#        return -1
-#    ml = []
-#    start = 0
-#    while start < len(haystack):
-#        index = haystack.find(needle, start)
-#        if index == -1:
-#            break
-#        ml.append(index)
-#        start = index + 1 
-#    return ml
-#
-#
-#haystack = "sadbutsad"
-#needle = "sad"
-#print(get_index(haystack, needle))
-#LeetCode 69
-#
-#def sqrt(x):
-#    if x == 0:
-#        return 0
-#    left = 1
-#    right = x
-#    while  left  < right:
-#        mid = left + (right - left) // 2
-#        if mid * mid == x:
-#            return mid
-#        elif mid  * mid < x:
-#            left = mid  + 1
-#        else:
-#            right = mid - 1 
-#
-#    return right 
-#
-#print(sqrt(4))
-#print(sqrt(8))
-#print(sqrt(81))
-#print(sqrt(625))
 
-#LeetCode 202
-#
-#def h_num(n):
-#    ms = set()
-#    while n != 1:
-#        n = sum(int(i)**2 for i in str(n))
-#        if n in ms:
-#            return False
-#        ms.add(n)
-#    return True
-#print(h_num(19))
-#print(h_num(2))
-#print(h_num(31))
-#print(h_num(41))
+def get_index(haystack, needle):
+    if not needle:
+        return [i for i in range(len(haysack) + 1)]
+    if needle not in haystack:
+        return -1
+    ml = []
+    start = 0
+    while start < len(haystack):
+        index = haystack.find(needle, start)
+        if index == -1:
+            break
+        ml.append(index)
+        start = index + 1 
+    return ml
+
+
+haystack = "sadbutsad"
+needle = "w"
+print(get_index(haystack, needle))
